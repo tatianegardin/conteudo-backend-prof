981244ca22481d2ff9178b2b73bf5c9679345fb5
tatianegardin <contatotatianegardin@hotmail.com>
2022-08-11 20:38:54 -0300

    conteudo prof

--- Proxy_pattern/Proxy_pattern/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Date;
-
-public class Main {
-    public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa("Batata", "Frita", "74185296312", new Date(), "Pfizer");
-        Vacinar vacinar = new ServicoVacinarProxy();
-        vacinar.vacinarPessoa(pessoa.getRg(), pessoa.getDataVacina(), pessoa.getNomeVacina());
-    }
-}
